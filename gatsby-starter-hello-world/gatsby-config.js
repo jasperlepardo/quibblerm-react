3d2e6493984bf5bd3f9f19b453a48faed954c4a5
Jasper Lepardo <jsprlprd@gmail.com>
2023-08-24 04:29:55 +0800

    sync

---
+++ gatsby-starter-hello-world/gatsby-config.js
@@ -0,0 +1,33 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Quicksand`,
+            file: `https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+            `,
+          },
+          {
+            name: `Commissioner`,
+            file: `    <link href="https://fonts.googleapis.com/css2?family=Commissioner:wght@100;200;300;400;500;600;700;800&family" rel="stylesheet">
+`,
+          },
+        ],
+      },
+    },
+  ],
+}
